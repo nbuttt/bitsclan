f4108a157a356f4ff65a423e2a6b230ac388d47d
Nashroz <tt384937@gmail.com>
2023-04-19 15:04:29 +0500

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,45 +13,7 @@ import { createApp  } from 'vue';
  * to use in your application's views. An example is included for you.
  */
 
-const app = createApp({
-
-    el: '#app',
-
-    //Store chat messages for display in this array.
-    data: {
-        messages: []
-    },
-    //Upon initialisation, run fetchMessages(). 
-    created() {
-        this.fetchMessages();
-
-        window.Echo.private('chat')
-        .listen('MessageSent', (e) => {
-            this.messages.push({
-            message: e.message.message,
-            user: e.user
-            });
-        });
-    },
-    methods: {
-        fetchMessages() {
-            //GET request to the messages route in our Laravel server to fetch all the messages
-            axios.get('/messages').then(response => {
-                //Save the response in the messages array to display on the chat view
-                this.messages = response.data;
-            });
-        },
-        //Receives the message that was emitted from the ChatForm Vue component
-        addMessage(message) {
-            //Pushes it to the messages array
-            this.messages.push(message);
-            //POST request to the messages route with the message data in order for our Laravel server to broadcast it.
-            axios.post('/messages', message).then(response => {
-                console.log(response.data);
-            });
-        }
-    }
-});
+const app = createApp({});
 import ExampleComponent from './components/ExampleComponent.vue';
 import chat_messages from './components/ExampleComponent.vue';
 import chat_form from './components/ExampleComponent.vue';
--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">How To Build Live Chat App In Laravel 9 And Vue JS - Websolutionstuff</div>
+                <div class="panel-heading">Bitsclan Chat</div>
 
                 <div class="panel-body">
                     <chat-messages :messages="messages"></chat-messages>
